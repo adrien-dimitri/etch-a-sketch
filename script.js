b0dc7828891018f1786b26a990ae1d40ff7b5821
adrien-dimitri <amaugueret@gmail.com>
2021-11-20 21:01:22 +0100

    added reset button functionality

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-for (i=1; i<17; i++) {
+function sketch(size){
     const container = document.querySelector("#container");
-    const row = document.createElement("div");
-    row.classList.add(`row`);
-    container.appendChild(row);
+    for (i=1; i<=size; i++) {
+        
+        const row = document.createElement("div");
+        row.classList.add(`row`);
+        container.appendChild(row);
+    }
+    
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(e => {
+        for (i=1; i<=size; i++) {
+            const square = document.createElement("div");
+            square.classList.add(`square`);
+            e.appendChild(square);
+        }
+    });
+    
+    const squares = document.querySelectorAll(".square");
+    
+    squares.forEach((e) => {
+        e.addEventListener("mousemove", () => {
+            console.log(e);
+            e.setAttribute("style", "background: black");
+        });
+    
+    });
+    
+    const button = document.querySelector("#button");
+    
+    button.addEventListener("click", () => {
+        squares.forEach((e) => {
+            e.removeAttribute("style", "background: black");
+        });
+        while(container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
+        const input = prompt("Input size of grid");
+        sketch(input);
+    
+    });
 }
 
-const rows = document.querySelectorAll(".row");
-rows.forEach(e => {
-    for (i=1; i<17; i++) {
-        const square = document.createElement("div");
-        square.classList.add(`square`);
-        e.appendChild(square);
-    }
-});
-
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((e) => {
-    e.addEventListener("mousemove", () => {
-        console.log(e);
-        e.setAttribute("style", "background: black");
-    });
-
-});
-
+sketch(16);
