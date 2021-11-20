d843ee626288fc445cf0c09330374e9f91f86b33
adrien-dimitri <amaugueret@gmail.com>
2021-11-20 19:49:38 +0100

    added HTML Boilerplate and container, used script to create square, and added simple sstyle

---
+++ script.js
@@ -0,0 +1,15 @@
+for (i=1; i<17; i++) {
+    const container = document.querySelector("#container");
+    const row = document.createElement("div");
+    row.classList.add(`row`);
+    container.appendChild(row);
+}
+
+const rows = document.querySelectorAll(".row");
+rows.forEach(e => {
+    for (i=1; i<17; i++) {
+        const square = document.createElement("div");
+        square.classList.add(`square`);
+        e.appendChild(square);
+    }
+});
