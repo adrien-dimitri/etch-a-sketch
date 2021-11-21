65c96be492cc03611e2e3bc6fb8c22f2cfef50d2
adrien-dimitri <amaugueret@gmail.com>
2021-11-21 13:05:47 +0100

    fixed prompt issue with CreateGrid function. Improved input test

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 function sketch(size){
-    const container = document.querySelector("#container");
-    for (i=1; i<=size; i++) {
-        
-        const row = document.createElement("div");
-        row.classList.add(`row`);
-        container.appendChild(row);
-    }
-    
-    const rows = document.querySelectorAll(".row");
-    rows.forEach(e => {
-        for (i=1; i<=size; i++) {
-            const square = document.createElement("div");
-            square.classList.add(`square`);
-            e.appendChild(square);
-        }
+    createGrid(size) //call createGrid() to create all the square from the given size
+
+    const button = document.querySelector("#button");    // reset button + input for size
+    button.addEventListener("click", function() {   
+       while(container.firstChild) {
+        container.removeChild(container.lastChild);
+       }
+       let input = inputTest();
+       createGrid(input);
     });
-    
-    const squares = document.querySelectorAll(".square");
-    
-    squares.forEach((e) => {
-        e.addEventListener("mousemove", () => {
-            console.log(e);
-            e.setAttribute("style", "background: black");
-        });
-    
-    });
-    
-    const button = document.querySelector("#button");
-    
-    button.addEventListener("click", () => {
+
+    function createGrid(size) { //function to create the grid
+        const container = document.querySelector("#container"); //creating rows
+        for (i=1; i<=size; i++) { 
+            const row = document.createElement("div");
+            row.classList.add(`row`);
+            container.appendChild(row);
+        } 
+        const rows = document.querySelectorAll(".row"); //creating squares in each row
+        rows.forEach(e => {
+            for (i=1; i<=size; i++) {
+                const square = document.createElement("div");
+                square.classList.add(`square`);
+                e.appendChild(square);
+            }
+        });      
+        const squares = document.querySelectorAll(".square"); //Listen to mouse hover event
         squares.forEach((e) => {
-            e.removeAttribute("style", "background: black");
+            e.addEventListener("mousemove", () => {
+                e.setAttribute("style", "background: black");
+            });
         });
-        while(container.firstChild) {
-            container.removeChild(container.lastChild);
-        }
-        const input = prompt("Input size of grid");
-        sketch(input);
-    
-    });
+    }
 }
 
-sketch(16);
+function inputTest() { //simple input test
+    const input = (prompt("Input size of grid"));
+    if (input > 0 || input < 101) {
+        return input; 
+    }
+    alert("Invalid input. Please Enter a value between 1 and 100.");
+        inputTest()
+    
+}
+
+sketch(16); //first generated grid
