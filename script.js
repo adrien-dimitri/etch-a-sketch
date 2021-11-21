5c85e1366f7f8beeb91571c77db0ee944914df1b
adrien-dimitri <amaugueret@gmail.com>
2021-11-21 16:23:50 +0100

    minor fixes to input test to exit program

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function sketch(size){
 
     const button = document.querySelector("#button");    // reset button + input for size
     button.addEventListener("click", function() {   
+       let input = inputTest();
        while(container.firstChild) {
         container.removeChild(container.lastChild);
        }
-       let input = inputTest();
        createGrid(input);
     });
 
@@ -33,13 +33,13 @@ function sketch(size){
         const z = Math.floor(Math.random()*255);
 
         squares.forEach((e) => {
-            e.setAttribute("style", `background: rgba(${x},${y},${z},0%)`)
+            e.setAttribute("style", `background: rgba(${x},${y},${z},0%)`) //set the bg colour of all square to be 0% opacity
             let opacity = 0;
             e.addEventListener("mouseover", () => {   
-                if (e.getAttribute("style") ==`background: rgba(${x},${y},${z},100%)`) {
+                if (e.getAttribute("style") ==`background: rgba(${x},${y},${z},100%)`) { //check if opacity is full
                     return;
                 }
-                if (e.getAttribute("style") ==`background: rgba(${x},${y},${z},${opacity}%)`) {
+                if (e.getAttribute("style") ==`background: rgba(${x},${y},${z},${opacity}%)`) { //if not full, add 20% to opacity
                     opacity += 20;
                     e.setAttribute("style", `background: rgba(${x},${y},${z},${opacity}%)`); 
                 };
@@ -49,12 +49,14 @@ function sketch(size){
 };
 
 function inputTest() { //simple input test
-    const input = (prompt("Input size of grid"));
+    const input = (prompt("Input size of grid. \nEnter 0 to exit."));
+    if (input == 0) {return alert("Thank you for playing :)")}
     if (input > 0 && input < 101) {
         return input; 
     };
-    alert("Invalid input. Please Enter a value between 1 and 100.");
-        inputTest()
+    alert("Invalid input. Please Enter a value between 1 and 100. \nEnter 0 to exit.");
+    if (input == 0) {return alert("Thank you for playing :)")}    
+    inputTest()
     
 };
 
