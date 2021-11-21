cb45ab92a8616b75e9629eb0410bfedae0757a85
adrien-dimitri <amaugueret@gmail.com>
2021-11-21 15:56:52 +0100

    Changed bg colour-change to random rgb with increasing opacity with each hover

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function sketch(size){
             row.classList.add(`row`);
             container.appendChild(row);
         } 
+        
         const rows = document.querySelectorAll(".row"); //creating squares in each row
         rows.forEach(e => {
             for (i=1; i<=size; i++) {
@@ -25,23 +26,41 @@ function sketch(size){
                 e.appendChild(square);
             }
         });      
+        
         const squares = document.querySelectorAll(".square"); //Listen to mouse hover event
+        const x = Math.floor(Math.random()*255);
+        const y = Math.floor(Math.random()*255);
+        const z = Math.floor(Math.random()*255);
+
         squares.forEach((e) => {
-            e.addEventListener("mousemove", () => {
-                e.setAttribute("style", "background: black");
+            e.setAttribute("style", `background: rgba(${x},${y},${z},0%)`)
+            let opacity = 0;
+            e.addEventListener("mouseover", () => {   
+                if (e.getAttribute("style") ==`background: rgba(${x},${y},${z},100%)`) {
+                    return;
+                }
+                if (e.getAttribute("style") ==`background: rgba(${x},${y},${z},${opacity}%)`) {
+                    opacity += 20;
+                    e.setAttribute("style", `background: rgba(${x},${y},${z},${opacity}%)`); 
+                };
             });
         });
-    }
-}
+    };
+};
 
 function inputTest() { //simple input test
     const input = (prompt("Input size of grid"));
     if (input > 0 && input < 101) {
         return input; 
-    }
+    };
     alert("Invalid input. Please Enter a value between 1 and 100.");
         inputTest()
     
+};
+
+function getRandomValue() {
+    const value = Math.floor(Math.random()*255);
+    return value;
 }
 
 sketch(16); //first generated grid
